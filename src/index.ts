130a2ad20ba5cfd44cb6c40eb1cd42db8e54b034
frostime <frostime@foxmail.com>
2025-01-04 20:09:24 +0800

    ✨ feat: js & device value

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
  * @Author       : frostime
  * @Date         : 2024-10-09 21:09:34
  * @FilePath     : /src/index.ts
- * @LastEditTime : 2025-01-01 17:11:40
+ * @LastEditTime : 2025-01-04 20:00:27
  * @Description  : 
  */
 
@@ -11,19 +11,19 @@ export * from "./settings";
 export * from "./types";
 
 export * as api from "./api";
+export * from "./block";
 export * from "./const";
 export * from "./dailynote";
 export * from "./dialog";
+export * from "./documents";
 export * from "./lute";
+export * from "./javascripts";
 
 export * from "./misc";
 export * from "./performance";
 export * from "./plugin-instance";
+export * from "./search";
 export * from "./siyuan-instance";
 
 export * from "./style";
 export * from "./time";
-
-export * from "./block";
-export * from "./documents";
-export * from "./search";
--- src/settings/local-storage.ts
+++ src/settings/local-storage.ts
@@ -3,14 +3,15 @@
  * @Author       : frostime
  * @Date         : 2024-07-13 21:19:23
  * @FilePath     : /src/settings/local-storage.ts
- * @LastEditTime : 2024-12-19 21:36:44
+ * @LastEditTime : 2025-01-04 20:04:09
  * @Description  : 
  */
 import { Plugin } from "siyuan";
+import { deepMerge } from "./utils";
 
-export const useLocalDeviceStorage = async (plugin: Plugin) => {
+export const useLocalDeviceStorage = async (plugin: Plugin, filePrefix: string = 'Device') => {
     const device = window.siyuan.config.system;
-    const fname = `Device@${device.id}.json`;
+    const fname = `${filePrefix || 'Device'}@${device.id}.json`;
     let config = await plugin.loadData(fname);
     config = config || {};
     return {
@@ -43,3 +44,22 @@ export const useLocalDeviceStorage = async (plugin: Plugin) => {
     }
 }
 
+
+export const useDevicewiseValue = <T>(defaultValue: T) => {
+    const device = window.siyuan.config.system;
+    let storage: Record<string, T> = {};
+    storage[device.id] = defaultValue;
+
+    return {
+        storage,
+        init: (values: Record<string, T>) => {
+            storage = deepMerge(storage, values);
+        },
+        get: () => {
+            return storage[device.id];
+        },
+        set: (value: T) => {
+            storage[device.id] = value;
+        }
+    }
+}
