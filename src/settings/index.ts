3734a8341b065c9c21908ee0255c8a4def30e2d3
frostime <frostime@foxmail.com>
2025-01-06 20:52:29 +0800

    export

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -3,11 +3,12 @@
  * @Author       : frostime
  * @Date         : 2024-12-18 20:45:54
  * @FilePath     : /src/settings/index.ts
- * @LastEditTime : 2024-12-18 20:46:06
+ * @LastEditTime : 2025-01-06 20:52:12
  * @Description  : 
  * 
  */
-export * from "./setting-utils";
-export * from "./utils";
 export * from "./local-storage";
+export * from "./setting-utils";
+export * from "./setting-adpater";
 export * from "./types";
+export * from "./utils";
